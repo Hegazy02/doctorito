03135bbeefa9e2f6f77130c055a1e70e3cd6e195
Hegazy02 <Abdelrhmanhegazy02@gmail.com>
2023-11-23 00:59:11 +0200

    create home board view

---
+++ lib/core/theme/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static TextStyle style30 = TextStyle(
+      fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue[600]);
+  static TextStyle style12 = TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.withOpacity(0.5));
+  static const TextStyle style16 =
+      TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white);
+}
